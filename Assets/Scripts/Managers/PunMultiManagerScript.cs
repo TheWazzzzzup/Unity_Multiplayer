53295cf48a152bea44991fe81b7bddd8cccfd66c
TheWazzzzzup <amitmoryosef@hotmail.com>
2023-04-03 20:20:52 +0300

    RoomCheck
    this is lior's ip

--- Assets/Scripts/Managers/PunMultiManagerScript.cs
+++ Assets/Scripts/Managers/PunMultiManagerScript.cs
@@ -8,6 +8,8 @@ using Photon.Realtime;
 
 public class PunMultiManagerScript : MonoBehaviourPunCallbacks
 {
+    [SerializeField] TMP_InputField nicknameInputField;
+
     [Header("Room Controls")]
     [SerializeField] private Button roomButton;
     [SerializeField] private string roomName;
@@ -17,9 +19,12 @@ public class PunMultiManagerScript : MonoBehaviourPunCallbacks
     [SerializeField] private TextMeshProUGUI m_tmpg_Room;
     [SerializeField] private TextMeshProUGUI m_tmpg_RoomName;
     [SerializeField] private TextMeshProUGUI m_tmpg_RoomPlayerCount;
+    [SerializeField] private TextMeshProUGUI m_tmpg_PlayerListText;
 
     public void PhotonPunLogin()
     {
+        PhotonNetwork.NickName = nicknameInputField.text;
+        Debug.Log($"Your nickname is {PhotonNetwork.NickName}");
         PhotonNetwork.ConnectUsingSettings();
     }
 
@@ -34,6 +39,7 @@ public class PunMultiManagerScript : MonoBehaviourPunCallbacks
         base.OnConnectedToMaster();
         Debug.Log("<color=#00ff00>Master Is Connected</color>");
         roomButton.interactable = true;
+        PhotonNetwork.JoinLobby();
     }
 
     public override void OnCreatedRoom()
@@ -76,23 +82,44 @@ public class PunMultiManagerScript : MonoBehaviourPunCallbacks
         m_tmpg_Room.text = "Failed To Connect To Room";
     }
 
+    [ContextMenu("RoomCount")]
+    public void GetRoomCount() => Debug.Log(PhotonNetwork.CountOfRooms);
+
     private void Update()
     {
         m_tmpg_Master.text = PhotonNetwork.NetworkClientState.ToString();
     }
 
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        Debug.Log("OnRoomListUpdateCalled");
+        base.OnRoomListUpdate(roomList);
+        var x = 0;
+        foreach (var roominfo in roomList)
+        {
+            x++;
+            Debug.Log($"{x}.{roominfo.Name}");
+        }
+    }
+
     void RefreshRoomUI()
     {
+        Debug.Log(PhotonNetwork.CountOfRooms);
+        m_tmpg_PlayerListText.text = null;
         if (PhotonNetwork.CurrentRoom != null)
         {
             m_tmpg_RoomName.text = PhotonNetwork.CurrentRoom.Name;
             m_tmpg_RoomPlayerCount.text = string.Format($"{PhotonNetwork.CurrentRoom.PlayerCount}/{PhotonNetwork.CurrentRoom.MaxPlayers}");
+            foreach (var player in PhotonNetwork.PlayerList)
+            {
+                m_tmpg_PlayerListText.text += player.NickName + "\n";
+            }
         }
     }
 
     public void CreateOrJoinRoom()
     {
         roomButton.interactable = false;
-        PhotonNetwork.JoinOrCreateRoom(roomName,new RoomOptions() { MaxPlayers = 2},null);
+        PhotonNetwork.JoinOrCreateRoom(roomName, new RoomOptions() { MaxPlayers = 20 },null);
     }
 }
